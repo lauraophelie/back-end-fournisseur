6082dc597ffe1131c80899c97725155bde411a2e
Laura Ophélie <lauraophelie1@gmail.com>
2023-11-17 09:15:54 +0100

    first commit

---
+++ fournisseur/src/main/java/com/systeme/fournisseur/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.systeme.fournisseur;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(FournisseurApplication.class);
+	}
+
+}
